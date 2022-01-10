032f7b0e1ccfb2f428b4e574d9ad0310049c77e5
yattdev <yattdeveloper@gmail.com>
2022-01-10 22:55:39 +0100

    Update users/__init__.py to import all its modules

---
+++ users/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from users import (urls, models, factories, apps, admin, views, tests)
+
+__all__ = [urls, models, factories, apps, admin, views, tests]

6fc2c243a4014f0d5e6d7aef9ba78410553d2c01
yattdev <yattdeveloper@gmail.com>
2022-01-10 22:52:17 +0100

    Update api/__init__.py to import all its models

---
+++ api/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from api import (admin, urls, models, apps, permissions, serializers, views,
+                 tests)
+
+__all__ = [admin, urls, models, apps, permissions, serializers, views, tests]

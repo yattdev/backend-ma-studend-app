3d45c83297de9212b5b021ece13f17695c0a03b2
yattdev <yattdeveloper@gmail.com>
2022-01-10 22:38:43 +0100

    Update api app to add appartment viewsets endpoints path to urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,16 @@
 
 from django.urls import path, include
 from .views.utils_view import get_csrf_token
+from .views.appartment_views import AppartmentViewSet
 
 from rest_framework.routers import SimpleRouter
+
+# -------------------------
+# student application endpoints
+router = SimpleRouter()
+
+# Appartment List/Details endpoints
+router.register('appartments', AppartmentViewSet, basename="appartments")
 
 urlpatterns = [
     path('get-token/', get_csrf_token, name="get-token"),  # to get csrf_token
@@ -12,4 +20,4 @@ urlpatterns = [
     path("auth", include("rest_framework.urls")),
     path('auth', include('djoser.urls'), name="auth"),
     path('auth', include('djoser.urls.jwt')),
-]
+] + router.urls

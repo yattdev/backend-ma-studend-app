6aabb459774535037a6c99a8c14b0a819d4a650e
yattdev <yattdeveloper@gmail.com>
2022-02-02 18:06:48 +0100

    Update student/factories/__init__.py to import All factories class on it

--- student/factories/__init__.py
+++ student/factories/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from student.factories import appartment_factory
-
-__all__ = [appartment_factory]
+from student.factories.appartment_factory import AppartementFactory
+from student.factories.student_factory import StudentFactory
+from student.factories.utils_models_factory import (CommunauteFactory,
+                                                    AllowedNumberFactory)

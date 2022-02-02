d309473978d275eabb8473fd8ba2442f89390561
yattdev <yattdeveloper@gmail.com>
2022-02-02 18:53:55 +0100

    Create a setup file in project to handle it has module
    - Allow modules inside project able to work together
    - Update requirements.txt with pip freeze

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ma-student',
+    version='1.0',
+    description='A Django RestFul API For appartment locaiton',
+    author='Alassane Yattara',
+    author_email='alassane@yatt.tech',
+    url='https://student.remote.yatt.tech/api/v1/docs/',
+    packages=find_packages(),
+)
